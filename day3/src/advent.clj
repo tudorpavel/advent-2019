956aba6cbf53714a877bf6057cdb6bad018ddfee
Tudor Pavel <tudorv.pavel@gmail.com>
2019-12-04 00:50:57 +0200

    day3 - part2
    Not surprisingly quite a bit of the code had to change from the implementation for part1:
      - use hashmaps for matrix values `{:wire1 15, :wire2 15}` to keep count of steps taken by each wire
      - change tests to adapt to the new matrix values
      - fix bug at `coord-range`, order matters for counting steps
      - use a better rule in `intersection-points` to find intersection between the 2 different wires

--- day3/src/advent.clj
+++ day3/src/advent.clj
@@ -18,55 +18,71 @@
   (let [[x y] (map read-string (clojure.string/split str #","))]
     {:x x, :y y}))
 
-(defn draw-segment [start end update-op hash]
+(defn draw-segment [start end wire-id hash]
   (defn coord-range [a b]
     (if (< a b)
       (range (inc a) (inc b))
-      (range b a)))
-  (defn update-point [h p]
-    (let [s (point->str p)]
-      (assoc h s (update-op (h s 0)))))
+      (reverse (range b a))))
+  (defn update-point [acc p]
+    (let [[step-count h] acc
+          s (point->str p)
+          new-step-count (inc step-count)]
+      (if (get-in h [s wire-id])
+        [new-step-count h]
+        [new-step-count (assoc h s (conj (h s {}) {wire-id new-step-count}))]
+        )
+      ))
   (cond
-    (= (:x start) (:x end)) (reduce update-point hash
-                                    (map (fn [y] {:x (:x start), :y y})
-                                         (coord-range (:y start) (:y end))))
-    (= (:y start) (:y end)) (reduce update-point hash
-                                    (map (fn [x] {:x x, :y (:y start)})
-                                         (coord-range (:x start) (:x end))))
+    (= (:x start) (:x end)) (second
+                             (reduce update-point
+                                     [(get-in hash [(point->str start) wire-id] 0) hash]
+                                     (map (fn [y] {:x (:x start), :y y})
+                                          (coord-range (:y start) (:y end)))))
+    (= (:y start) (:y end)) (second
+                             (reduce update-point
+                                     [(get-in hash [(point->str start) wire-id] 0) hash]
+                                     (map (fn [x] {:x x, :y (:y start)})
+                                          (coord-range (:x start) (:x end)))))
     :else (throw (AssertionError. "Given segment is diagonal?"))))
 
-(defn trace [wire current update-op hash]
+(defn trace [wire current wire-id hash]
   (let [step (first wire)
         next (next-point current step)]
     (if (empty? (rest wire))
-      (draw-segment current next update-op hash)
-      (recur (rest wire) next update-op (draw-segment current next update-op hash)))
+      (draw-segment current next wire-id hash)
+      (recur (rest wire) next wire-id (draw-segment current next wire-id hash)))
     ))
+
+(defn intersection-points [hash]
+  "intersection-points returns a list of the form
+  (["6,5" {:wire1 15, :wire2 15}] ["3,3" {:wire1 20, :wire2 20}])"
+
+  (filter (fn [h] (every? #(contains? (second h) %) [:wire1 :wire2]))
+          hash))
 
 (defn manhattan-distance-from-origin [hash]
   (defn actual-distance [p]
     (+ (Math/abs (:x p))
        (Math/abs (:y p))))
-  (let [intersection-points (filter #(= (second %) 16) hash)]
-    ;; intersection-points is a list of the form (["6,5" 2] ["3,3" 4])
-    (apply min (map (comp actual-distance str->point first) intersection-points))
-    ))
+
+  (apply min (map (comp actual-distance str->point first) (intersection-points hash)))
+  )
+
+(defn trace-wires [wire1 wire2]
+  (trace wire2 {:x 0, :y 0} :wire2
+         (trace wire1 {:x 0, :y 0} :wire1 {})))
 
 (defn solve1 [input]
   (let [[wire1, wire2] input]
-    ;; desperate solution: self intersections will be 6 for wire1 and 26 for wire2.
-    ;; the intersections we care about will have value exactly 16
-    (manhattan-distance-from-origin (trace wire2
-                                           {:x 0, :y 0}
-                                           #(+ % 13)
-                                           (trace wire1
-                                                  {:x 0, :y 0}
-                                                  #(+ % 3)
-                                                  {})))
-    ))
+    (manhattan-distance-from-origin (trace-wires wire1 wire2))))
 
 (defn solve2 [input]
-  "Soon.")
+  (defn total-steps [intersection-point]
+    (let [h (second intersection-point)]
+      (+ (:wire1 h) (:wire2 h))))
+
+  (let [[wire1, wire2] input]
+    (apply min (map total-steps (intersection-points (trace-wires wire1 wire2))))))
 
 (defn read-input []
   (map #(clojure.string/split % #",") (clojure.string/split-lines (slurp *in*))))
