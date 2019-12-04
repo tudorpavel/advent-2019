6724ac84041c3c1cf341a2cd0445660126ccd41f
Tudor Pavel <tudorv.pavel@gmail.com>
2019-12-04 21:12:11 +0200

    day4 - part2

--- day4/src/advent.clj
+++ day4/src/advent.clj
@@ -5,24 +5,32 @@
     (conj (digits (quot n 10)) (mod n 10))
     []))
 
-(defn same-adjacent? [digs]
-  (let [[h & t] digs]
-    (cond
-      (empty? t) false
-      (= h (first t)) true
-      :else (recur t))))
+(defn count-digits [digs]
+  (reduce #(update %1 %2 inc)
+          (vec (repeat 10 0))
+          digs))
 
-(defn valid-password? [n]
+(defn valid-with-rule? [n digit-count-pred]
   (let [digs (digits n)]
     (and (= (sort digs) digs)
-         (same-adjacent? digs))))
+         (pos? (count (filter digit-count-pred
+                              (count-digits digs)))))))
+
+(defn loosely-valid? [n]
+  (valid-with-rule? n (partial < 1)))
+
+(defn strictly-valid? [n]
+  (valid-with-rule? n (partial = 2)))
+
+(defn password-count-with-rule [input rule-pred]
+  (let [[start end] input]
+    (count (filter rule-pred (range start (inc end))))))
 
 (defn solve1 [input]
-  (let [[start end] input]
-    (count (filter valid-password? (range start (inc end))))))
+  (password-count-with-rule input loosely-valid?))
 
 (defn solve2 [input]
-  "Soon.")
+  (password-count-with-rule input strictly-valid?))
 
 (defn read-input []
   (map read-string
--- day4/test/advent_test.clj
+++ day4/test/advent_test.clj
@@ -6,19 +6,35 @@
   (is (= [1 2 3] (digits 123)))
   )
 
-(deftest same-adjacent?-test
-  (is (same-adjacent? (digits 122345)))
-  (is (not (same-adjacent? (digits 153452))))
+(deftest count-digits-test
+  (is (= [0 4 2 0 0 0 0 0 0 0] (count-digits (digits 111122))))
+  (is (= [0 1 3 0 1 1 0 0 0 0] (count-digits (digits 122245))))
   )
 
-(deftest valid-password?-test
-  (is (valid-password? 111111))
-  (is (valid-password? 122345))
-  (is (valid-password? 111123))
-  (is (not (valid-password? 223450)))
-  (is (not (valid-password? 123789)))
+(deftest loosely-valid?-test
+  (is (loosely-valid? 111111))
+  (is (not (loosely-valid? 223450)))
+  (is (not (loosely-valid? 123789)))
+  (is (loosely-valid? 112233))
+  (is (loosely-valid? 123444))
+  (is (loosely-valid? 111122))
+  (is (not (loosely-valid? 153441)))
+  )
+
+(deftest strictly-valid?-test
+  (is (not (strictly-valid? 111111)))
+  (is (not (strictly-valid? 223450)))
+  (is (not (strictly-valid? 123789)))
+  (is (strictly-valid? 112233))
+  (is (not (strictly-valid? 123444)))
+  (is (strictly-valid? 111122))
+  (is (not (strictly-valid? 153441)))
   )
 
 (deftest solve1-test
-  (is (= 3 (solve1 (list 111123 111125))))
+  (is (= 4 (solve1 (list 111122 111125))))
+  )
+
+(deftest solve2-test
+  (is (= 1 (solve2 (list 111122 111125))))
   )
