077cd48f74b1c4da96ca844ebc03002e84494fd0
Tudor Pavel <tudorv.pavel@gmail.com>
2019-12-07 19:37:28 +0200

    day7 - part2
    Pretty messy solution for Day7 Part2 and now I have to fix Day2 and Day5.

--- day7/src/advent.clj
+++ day7/src/advent.clj
@@ -2,16 +2,43 @@
   (:require [clojure.math.combinatorics :as combo]
             [intcode-computer :as ic]))
 
-(defn thruster-signal [program phase-settings]
+(defn thruster-signal [initial-memory phase-settings]
   (reduce (fn [prev-output, phase-setting]
-            (first (:outputs
-                    (ic/run-program program [phase-setting prev-output]))))
+            (:output
+             (ic/run-program initial-memory
+                             [phase-setting prev-output])))
           0
           phase-settings))
+
+(defn thruster-signal-looped [initial-memory, phase-settings]
+  (with-local-vars [ampA (ic/init-program initial-memory [(phase-settings 0)])
+                    ampB (ic/init-program initial-memory [(phase-settings 1)])
+                    ampC (ic/init-program initial-memory [(phase-settings 2)])
+                    ampD (ic/init-program initial-memory [(phase-settings 3)])
+                    ampE (ic/init-program initial-memory [(phase-settings 4)])]
+    (var-set ampE (assoc @ampE :output 0))
+    (while (not= :EXIT (:output @ampE))
+      (do
+        (var-set ampA (update @ampA :inputs conj (:output @ampE)))
+        (var-set ampA (ic/exec-program @ampA))
+        (var-set ampB (update @ampB :inputs conj (:output @ampA)))
+        (var-set ampB (ic/exec-program @ampB))
+        (var-set ampC (update @ampC :inputs conj (:output @ampB)))
+        (var-set ampC (ic/exec-program @ampC))
+        (var-set ampD (update @ampD :inputs conj (:output @ampC)))
+        (var-set ampD (ic/exec-program @ampD))
+        (var-set ampE (update @ampE :inputs conj (:output @ampD)))
+        (var-set ampE (ic/exec-program @ampE))))
+
+    (first (:inputs @ampA))))
 
 (defn max-thruster-signal [program]
   (apply max (map (partial thruster-signal program)
                   (combo/permutations (range 5)))))
+
+(defn max-thruster-signal-looped [program]
+  (apply max (map (partial thruster-signal-looped program)
+                  (combo/permutations (range 5 10)))))
 
 (defn -main []
   (defn read-input []
@@ -19,4 +46,4 @@
 
   (let [program (ic/make-program (read-input))]
     (println "Part 1:" (max-thruster-signal program))
-    (println "Part 2:" "Soon.")))
+    (println "Part 2:" (max-thruster-signal-looped program))))
--- lib/intcode-computer/src/intcode_computer.clj
+++ lib/intcode-computer/src/intcode_computer.clj
@@ -4,10 +4,14 @@
   (let [[a b & rest] (reverse (str op))]
     (map (comp int bigint) (conj (map str rest) (str b a)))))
 
-(defn- compute-result
-  ([program inputs] (compute-result 0 inputs {:outputs [] :memory program}))
-  ([pos inputs result]
-   (let [{memory :memory} result
+(defn init-program [memory inputs]
+  {:pos 0 :memory memory
+   :inputs inputs :output nil})
+
+(defn exec-program
+  ([program]
+   (let [{pos :pos} program
+         {memory :memory} program
          [code & param-modes] (parse-op (memory pos))]
      (defn operand [mode p]
        (memory (if (= mode 1) p (memory p))))
@@ -21,47 +25,43 @@
               (memory (+ pos 3))
               (apply func (operands))))
 
-     (defn new-result [func]
-       (assoc result
-              :memory
-              (new-memory func)))
+     (defn new-program [func]
+       (assoc program
+              :pos (+ pos 4)
+              :memory (new-memory func)))
 
      (case code
-       1 (recur (+ pos 4) inputs (new-result +))
-       2 (recur (+ pos 4) inputs (new-result *))
-       3 (let [[input & inputs] inputs]
-           (recur (+ pos 2)
-                  inputs
-                  (assoc result :memory
-                         (assoc memory
-                                (memory (inc pos))
-                                input))))
+       1 (recur (new-program +))
+       2 (recur (new-program *))
+       3 (let [[input & inputs] (:inputs program)]
+           (recur (assoc program
+                         :pos (+ pos 2)
+                         :inputs inputs
+                         :memory (assoc memory
+                                        (memory (inc pos))
+                                        input))))
        4 (let [operand (operand (first param-modes) (inc pos))]
-           (recur (+ pos 2)
-                  inputs
-                  (update result
-                          :outputs
-                          #(conj % operand))) )
+           (assoc program :output operand :pos (+ pos 2)))
        5 (let [operands (operands)]
-           (recur (if (zero? (first operands))
-                    (+ pos 3)
-                    (second operands))
-                  inputs
-                  result))
+           (recur (assoc program
+                         :pos
+                         (if (zero? (first operands))
+                           (+ pos 3)
+                           (second operands)))))
        6 (let [operands (operands)]
-           (recur (if (zero? (first operands))
-                    (second operands)
-                    (+ pos 3))
-                  inputs
-                  result))
-       7 (recur (+ pos 4) inputs (new-result #(if (< %1 %2) 1 0)))
-       8 (recur (+ pos 4) inputs (new-result #(if (= %1 %2) 1 0)))
-       99 result
+           (recur (assoc program
+                         :pos
+                         (if (zero? (first operands))
+                           (second operands)
+                           (+ pos 3)))))
+       7 (recur (new-program #(if (< %1 %2) 1 0)))
+       8 (recur (new-program #(if (= %1 %2) 1 0)))
+       99 (assoc program :output :EXIT)
        (throw (AssertionError. "Unexpected instruction code."))))))
 
 (defn make-program [program-str]
   (mapv read-string (.split program-str ",")))
 
 (defn run-program
-  ([program] (run-program program []))
-  ([program inputs] (compute-result program inputs)))
+  ([memory] (run-program memory []))
+  ([memory inputs] (exec-program (init-program memory inputs))))
